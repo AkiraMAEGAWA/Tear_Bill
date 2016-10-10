15d4c21781cfa472399f2426cd21995ea5c65b12
AkiraMAEGAWA <maeaki112@gmail.com>
2016-10-11 04:38:29 +0900

    comment out

--- TearBill_Page/js/test.js
+++ TearBill_Page/js/test.js
@@ -2,5 +2,7 @@ $(function() {
     $('#hoge').on('click', function(){
         alert('ぬふ');
     });
+    /*  expand subheader
     $('.content-subhead').animate({ "height" : "130px" });
+    */
 });
